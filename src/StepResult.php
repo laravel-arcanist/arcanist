5df3a3bdad321ae2ea8f4499bc5308cb878e7243
Kai Sassnowski <me@kai-sassnowski.com>
2021-04-15 22:10:33 +0200

    Add StepResult class

---
+++ src/StepResult.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace Sassnowski\Arcanist;
+
+final class StepResult
+{
+    private function __construct(
+        private bool $successful,
+        private array $payload = [],
+        private ?string $error = null
+    ) {
+    }
+
+    public static function success(array $payload = []): self
+    {
+        return new self(true, payload: $payload);
+    }
+
+    public static function failed(?string $error = null): self
+    {
+        return new self(false, error: $error);
+    }
+
+    public function successful(): bool
+    {
+        return $this->successful;
+    }
+
+    public function payload(): array
+    {
+        return $this->payload;
+    }
+
+    public function error(): ?string
+    {
+        return $this->error;
+    }
+}
---
+++ tests/StepResultTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sassnowski\Arcanist\StepResult;
+
+class StepResultTest extends TestCase
+{
+    /** @test */
+    public function can_create_a_successful_result(): void
+    {
+        $result = StepResult::success();
+
+        self::assertTrue($result->successful());
+    }
+
+    /** @test */
+    public function can_create_a_failed_result(): void
+    {
+        $result = StepResult::failed();
+
+        self::assertFalse($result->successful());
+    }
+
+    /** @test */
+    public function can_pass_along_data_to_successful_result(): void
+    {
+        $result = StepResult::success(['::data::']);
+
+        self::assertEquals(['::data::'], $result->payload());
+    }
+
+    /** @test */
+    public function can_pass_along_error_message_to_failed_result(): void
+    {
+        $result = StepResult::failed('::message::');
+
+        self::assertEquals('::message::', $result->error());
+    }
+}
