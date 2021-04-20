17bdc6c5d6d4a24896ca766c996b56f480d29054
Kai Sassnowski <me@kai-sassnowski.com>
2021-04-20 07:53:13 +0200

    Add solution for StepTemplateNotFoundException

--- src/Exception/StepTemplateNotFoundException.php
+++ src/Exception/StepTemplateNotFoundException.php
@@ -4,11 +4,25 @@ namespace Sassnowski\Arcanist\Exception;
 
 use Exception;
 use Sassnowski\Arcanist\WizardStep;
+use Facade\IgnitionContracts\Solution;
+use Facade\IgnitionContracts\BaseSolution;
+use Facade\IgnitionContracts\ProvidesSolution;
 
-class StepTemplateNotFoundException extends Exception
+class StepTemplateNotFoundException extends Exception implements ProvidesSolution
 {
+    public function __construct(private WizardStep $step)
+    {
+        parent::__construct("No template found for step [{$this->step->slug}].");
+    }
+
     public static function forStep(WizardStep $step): self
     {
-        return new self("No template found for step [{$step->slug}].");
+        return new self($step);
+    }
+
+    public function getSolution(): Solution
+    {
+        return BaseSolution::create('No template for wizard step found')
+            ->setSolutionDescription("No template was found for the step [{$this->step->name}].");
     }
 }
